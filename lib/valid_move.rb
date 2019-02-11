57a5a7dbe5b4bd37563c2ba2dcc1d69834501fa2
Michael Nnaji <mnnaji3@uwo.ca>
2019-02-11 17:54:36 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -11,8 +11,8 @@ end
 
 def  valid_move?(board,index)
   index = index.to_i-1
-if index.between?(0,8) && position_taken?(board,index) == false
-  return true
+if index.between?(0,8) && position_taken?(board,index) == true
+  return false
 end
 end
 
